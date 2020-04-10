c78f77d4a2584453d9a7b15065801bdc53250796
Philip Jung <philip.jung.98@gmail.com>
2020-04-10 05:03:09 -0400

    modified TaskCard css

--- frontend/src/components/TaskCard.js
+++ frontend/src/components/TaskCard.js
@@ -4,16 +4,12 @@ function CalendarCard({task}) {
 
   return(
     <div> 
-      <div className="max-w-sm w-full lg:max-w-full mb-6">
-        <div className="border border-gray-400 lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-          <div className="mb-4">
-            <div className="text-gray-900 font-bold text-xl">{`${task.title}`}</div>
-            <div className="text-gray-700 text-base">{`${task.category}`}</div>
-          </div>
-          <div className="flex items-center">
-            <div className="text-sm">
-              <div className="text-gray-900 leading-none">{`${Math.floor(task.estimated_time/60)} hours ${Math.floor(task.estimated_time%60)} mins`}</div>
-            </div>
+      <div className="max-w-sm w-full lg:max-w-full mb-4">
+        <div className="border border-white bg-white rounded-md shadow-lg p-4 flex flex-col justify-between leading-normal">
+          <div className="my-2 flex items-center">
+            <div className="text-gray-900 font-bold text-xl w-4/12">{`${task.title}`}</div>
+            <div className="text-gray-700 text-base w-4/12">{`${task.category}`}</div>
+            <div className=" text-sm text-gray-900 leading-none w-4/12">{`${Math.floor(task.estimated_time/60)} hours ${Math.floor(task.estimated_time%60)} mins`}</div>
           </div>
         </div>
       </div>
