928c148aa585cd3452f4c7e4c9bbb6b87d1583f1
Philip Jung <philip.jung.98@gmail.com>
2020-05-06 03:56:02 -0400

    Updates

---
+++ precache-manifest.5c802343e8a4583f1e658b4228966c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb3ca02b4fa0509671346a3cfb5893e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b399f4a397c05a254f5",
+    "url": "/static/css/main.a8ae86ba.chunk.css"
+  },
+  {
+    "revision": "cb96020ee656c6581059",
+    "url": "/static/js/2.3a122077.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/static/js/2.3a122077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b399f4a397c05a254f5",
+    "url": "/static/js/main.143c79ab.chunk.js"
+  },
+  {
+    "revision": "7accac3f2464ed2289cf",
+    "url": "/static/js/runtime-main.55f5c344.js"
+  }
+]);
