604840b0fa46c908bdb49ea3a5e36e15a3334b53
Philip Jung <philip.jung.98@gmail.com>
2020-05-06 07:35:26 -0400

    heroku changes

---
+++ frontend/build/precache-manifest.9acf3dddf5c12b27079c707e84c2afcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db13f07a7fd62ba2b73abf8d80f0a74c",
+    "url": "/nibble/index.html"
+  },
+  {
+    "revision": "1fddb31c6109874951bf",
+    "url": "/nibble/static/css/main.0e581911.chunk.css"
+  },
+  {
+    "revision": "5d5c363d6eb39d7110e4",
+    "url": "/nibble/static/js/2.0625b6be.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/nibble/static/js/2.0625b6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fddb31c6109874951bf",
+    "url": "/nibble/static/js/main.57a9f2dd.chunk.js"
+  },
+  {
+    "revision": "2dbb1da6ae4a3f709bb2",
+    "url": "/nibble/static/js/runtime-main.72fca969.js"
+  }
+]);
