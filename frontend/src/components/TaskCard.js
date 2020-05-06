521a8804471185f68e02829cfec2f857457a6db2
Philip Jung <philip.jung.98@gmail.com>
2020-05-06 04:08:24 -0400

    github pages

--- frontend/src/components/TaskCard.js
+++ frontend/src/components/TaskCard.js
@@ -21,12 +21,13 @@ function TaskCard({task}) {
 
   return(
     <div className="max-w-sm w-full bg-white lg:max-w-full mb-4">
-      <div>{task.time_started ? timeElapsed.format("HH:mm:ss") : null}</div>
       <div className="border border-white rounded-md shadow-lg p-4 flex flex-col justify-between leading-normal">
         <div className="my-2 flex items-center">
           <div className="text-gray-900 font-bold text-xl w-4/12">{`${task.title}`}</div>
           <div className="text-gray-700 text-base w-4/12">{`${task.category}`}</div>
-          <div className=" text-sm text-gray-900 leading-none w-4/12">{`${Math.floor(task.estimated_time/60)} hours ${Math.floor(task.estimated_time%60)} mins`}</div>
+          <div className=" text-sm text-gray-900 leading-none w-4/12">
+            {task.time_started ? `${timeElapsed.format("HH:mm:ss")} /`: null} {moment.utc().hours(Math.floor(task.estimated_time/60)).minutes(Math.floor(task.estimated_time%60)).seconds(0).format("hh:mm:ss")}
+            </div>
         </div>
       </div>
     </div>
